4e79acdc5c8ff476e82433f986a29941024640dc
Neelima Benny <neelimabenny@209-137.priv23.nus.edu.sg>
2018-03-10 13:48:57 +0800

    updated the skeleton of participant class

--- TrailBlazeLearn/app/src/main/java/com/ft08/trailblazelearn/models/Participant.java
+++ TrailBlazeLearn/app/src/main/java/com/ft08/trailblazelearn/models/Participant.java
@@ -1,5 +1,27 @@
 package com.ft08.trailblazelearn.models;
 //Updated by Abhijit
 
-public class Participant {
+public class Participant extends User{
+
+    private String trailId;
+
+    Participant(){
+
+
+    }
+
+    public String getTrailId() {
+        return trailId;
+    }
+
+    public void setTrailId(String trailId) {
+        this.trailId = trailId;
+    }
+
+    public void joinTrail(String trailId){
+
+        //join trail using trailId
+    }
+
+
 }
