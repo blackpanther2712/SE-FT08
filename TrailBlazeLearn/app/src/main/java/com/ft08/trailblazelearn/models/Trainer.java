7987cc985288c13f5e8b7987b76b7b61d5cdb302
Afaque Ahmad <37176211+blackpanther2712@users.noreply.github.com>
2018-03-11 13:01:04 +0800

    Update Trainer.java

--- TrailBlazeLearn/app/src/main/java/com/ft08/trailblazelearn/models/Trainer.java
+++ TrailBlazeLearn/app/src/main/java/com/ft08/trailblazelearn/models/Trainer.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public class Trainer extends User {
 
-
-
     public Trainer(String userId, String name, String image) {
         super(userId, name, image);
         trails = new ArrayList<>();
@@ -17,25 +15,21 @@ public class Trainer extends User {
         return trails;
     }
 
-    public Trail addTrail(String trailName, String module, Date trailDate){
+    public Trail addTrail(String trailName, String module, Date trailDate) {
         Trail trail = new Trail(trailName,module,trailDate);
         trails.add(trail);
         return trail;
     }
-    public void removeTrail(String trailID){
+    public void removeTrail(String trailID) {
         Trail trail = getTrail(trailID);
         if(trail!=null){
             trails.remove(trail);
         }
     }
 
-    public Trail editTrail(String trailName, String module, Date trailDate,String trailID){
+    public Trail editTrail(String trailName, String module, Date trailDate,String trailID) {
         Trail trail = getTrail(trailID);
         return trail.editTrail(trailName,module,trailDate);
 
     }
-
-
-
-
 }
